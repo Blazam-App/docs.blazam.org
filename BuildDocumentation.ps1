91ce8d3d4bb3545569fe73b115a8b4aada83c58b
Chris Jacobsen Work <cjacobsen@work.com>
2024-02-09 14:37:40 -0500

    Update edit url

--- BuildDocumentation.ps1
+++ BuildDocumentation.ps1
@@ -2,8 +2,4 @@
 Set-Location $ProjectDir
 mkdocs build
 
-<<<<<<< HEAD
-copy CNAME docs/CNAME
-=======
 Copy-Item -Path "CNAME" -Destination "docs/CNAME"
->>>>>>> 63d2e0afaca7ea00244d1b7176237a2482099934
